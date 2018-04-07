577567024a79476800d4f5590afccc633fe6cd97
greennoizuf <zebm54@bigpond.com>
2018-04-07 12:22:27 +1000

    will remove users donation on loadUser now

--- unlisted/mysql_functions.js
+++ unlisted/mysql_functions.js
@@ -88,8 +88,12 @@ module.exports = function () {
                     }
                     if(res[0].tier)
                         user.donationTier = res[0].tier;
+                    else if(user.donationTier)
+                        user.donationTier = null;
                     if(res[0].expires)
                         user.donationExpires = res[0].expires;
+                    else if(user.donationExpires)
+                        user.donationExpires = null;
                     resolve(conn);
                 });
             }); 
