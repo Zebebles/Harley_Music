df7880ff99eb072bd5c75f56f012dc440a1a590e
greennoizuf <zebm54@bigpond.com>
2018-04-18 15:25:14 +1000

    prev shouldw ork

---
+++ commands/music - prev.js
@@ -0,0 +1,28 @@
+const DBF = require('discordjs-bot-framework');
+
+
+module.exports = class Hello extends DBF.Command{
+    constructor(){
+        super({
+             name: "previous",
+             triggers: ["prev", "back", "last"],
+             group: "Music",
+             ownerOnly : false,
+             description: "Go backwards in the queue.",
+             example: ">>prev number_of_tracks",             
+             guildOnly : true,
+             reqArgs : true,
+        });
+    }
+    run(params = {msg, args}){
+        let msg = params.msg; let args = params.args;
+        let incrementN;
+        let validation = msg.guild.playlist.validateCommand(msg,true);
+        if(validation)
+            return msg.channel.send(validation).catch(err => console.log(err));
+        incrementN = parseInt(args);
+        msg.channel.send(`:track_next: Going back \`${incrementN+1}\` tracks as requested by **${msg.member.displayName}**`).catch(err => console.log(err));
+        msg.guild.playlist.queue.prev(isNaN(incrementN) ? null : incrementN);
+        msg.guild.playlist.dispatcher.destroy();
+    }
+}
