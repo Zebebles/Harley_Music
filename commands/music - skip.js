3856eadcf5519e58306c3ab0ae07c6e4f7052749
greennoizuf <zebm54@bigpond.com>
2018-04-18 15:41:59 +1000

    skip shal work

--- commands/music - skip.js
+++ commands/music - skip.js
@@ -25,10 +25,10 @@ module.exports = class Hello extends DBF.Command{
         
         if(incrementN > 0) 
         {
-            msg.channel.send(`:track_next: Skipping \`${incrementN+1}\` tracks as requested by **${msg.member.displayName}**`).catch(err => console.log(err));
-            msg.guild.playlist.queue.next(incrementN);    //  -1 BECAUSE ONE WILL BE SKIPPED ON PLAYLIST.NEXT();
+            msg.channel.send(`:track_next: Skipping \`${incrementN}\` tracks as requested by **${msg.member.displayName}**`).catch(err => console.log(err));
+            msg.guild.playlist.queue.next(incrementN-1);    //  -1 BECAUSE ONE WILL BE SKIPPED ON PLAYLIST.NEXT();
         }
-        
+
         msg.guild.playlist.dispatcher.destroy();
     }
 }
