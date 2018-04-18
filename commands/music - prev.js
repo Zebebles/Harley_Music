18d8209b64d2d822681ac2405479896cb69cc4db
greennoizuf <zebm54@bigpond.com>
2018-04-18 17:28:28 +1000

    only shows if > 1

--- commands/music - prev.js
+++ commands/music - prev.js
@@ -22,7 +22,11 @@ module.exports = class Hello extends DBF.Command{
             return msg.channel.send(validation).catch(err => console.log(err));
         incrementN = parseInt(args);
         incrementN = incrementN && incrementN > 0 ? incrementN : 1; 
-        msg.channel.send(`:track_next: Going back \`${msg.guild.playlist.queue.prev(incrementN)-1}\` tracks as requested by **${msg.member.displayName}**`).catch(err => console.log(err));
+        let skipped = msg.guild.playlist.queue.prev(incrementN)-1;
+        
+        if(skipped > 1)
+            msg.channel.send(`:track_next: Going back \`${skipped}\` tracks as requested by **${msg.member.displayName}**`).catch(err => console.log(err));
+        
         msg.guild.playlist.dispatcher.destroy();
     }
 }
