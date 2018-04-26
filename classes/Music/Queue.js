b137078c443ae632b75fea8a3712343b5451018f
greennoizuf <zebm54@bigpond.com>
2018-04-26 20:48:06 +1000

    commenting

--- classes/Music/Queue.js
+++ classes/Music/Queue.js
@@ -123,8 +123,7 @@ module.exports = class Queue
     }
 
     remove(song)    //  SONG CAN BE A VIDEO OR A STRING THAT IDENTIFIES THE VIDEO (TITLE)
-    {      
-                         //  OR AN ARRAY INDEX.  IF SONG IS AN ARRAY INDEX AND AMOUNT IS SET, THEN THE AMOUNT OF SONGS WILL BE REMOVED STARTING AT SONGS[SONG]
+    {               //  OR AN ARRAY INDEX OR A RANGE OF INDEXES I.E. 1-3
         let songs = this.songs.slice(this.index+1, this.songs.length);
         let removed;
         if(typeof song == "string" && song.match(/\d+-\d+/g))
